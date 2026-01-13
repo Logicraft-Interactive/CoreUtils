02c6afabde29301142858fa6166313c74b312605
sharllesse <charles19lesage06@gmail.com>
2026-01-13 21:44:03 +0100

    Applied requested change

--- Plugins/LogicraftCoreUtils/Source/LogicraftCoreUtils/Private/TimerHolder.cpp
+++ Plugins/LogicraftCoreUtils/Source/LogicraftCoreUtils/Private/TimerHolder.cpp
@@ -1,7 +1,12 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "TimerHolder.h"
+
+#define TIMER_HOLDER_CHECK \
+		checkf(TimerManager != nullptr, TEXT("The timer manager was not retrieved prior to using this function."));
+
+#define TIMER_HOLDER_ENSURE \
+		ensureMsgf(RetrieveTimerManager(), TEXT("Unable to retrieve the timer manager because no valid context was found."))
 
 FTimerHolder::~FTimerHolder()
 {
@@ -10,7 +15,7 @@ FTimerHolder::~FTimerHolder()
 
 void FTimerHolder::Pause()
 {
-	if (ensureMsgf(RetrieveTimerManager(), TEXT("Unable to retrieve the timer manager because no valid context was found.")))
+	if (TIMER_HOLDER_ENSURE)
 	{
 		TimerManager->PauseTimer(TimerHandle);
 	}
@@ -18,7 +23,7 @@ void FTimerHolder::Pause()
 
 void FTimerHolder::Clear()
 {
-	if (ensureMsgf(RetrieveTimerManager(), TEXT("Unable to retrieve the timer manager because no valid context was found.")))
+	if (TIMER_HOLDER_ENSURE)
 	{
 		TimerManager->ClearTimer(TimerHandle);
 	}
@@ -26,31 +31,31 @@ void FTimerHolder::Clear()
 
 bool FTimerHolder::IsPaused() const
 {
-	check(TimerManager != nullptr);
+	TIMER_HOLDER_CHECK
 	return TimerManager->IsTimerPaused(TimerHandle);
 }
 
 bool FTimerHolder::IsAlreadyRunning() const
 {
-	check(TimerManager != nullptr);
+	TIMER_HOLDER_CHECK
 	return TimerManager->TimerExists(TimerHandle);
 }
 
 float FTimerHolder::GetElapsedTime() const
 {
-	check(TimerManager != nullptr);
+	TIMER_HOLDER_CHECK
 	return TimerManager->GetTimerElapsed(TimerHandle);
 }
 
 float FTimerHolder::GetRate() const
 {
-	check(TimerManager != nullptr);
+	TIMER_HOLDER_CHECK
 	return TimerManager->GetTimerRate(TimerHandle);
 }
 
 float FTimerHolder::GetRemainingTime() const
 {
-	check(TimerManager != nullptr);
+	TIMER_HOLDER_CHECK
 	return TimerManager->GetTimerRemaining(TimerHandle);
 }
 
