e8655c6c28183b2c7bea5bad4e62257e34c0e90a
Charles Lesage <charles19lesage06@gmail.com>
2026-01-13 16:33:17 +0100

    Added some comment and need chain task

---
+++ Source/LogicraftCoreUtilsSB/Private/TestTimerHolder.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LogicraftCoreUtilsSB/Public/TestTimerHolder.h"
+
+#include "TimerHolderSubsystem.h"
+
+ATestTimerHolder::ATestTimerHolder()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ATestTimerHolder::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TimerHandle = UTimerHolderSubsystem::ScheduleTimer(this, []()
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Start timer log"))
+	}, { .Rate = 10.f });
+
+	UTimerHolderSubsystem::ScheduleTimer(this, []()
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Start timer log"))
+	}, { .Rate = 10.f }, ETimerScope::ContextBound);
+
+	UTimerHolderSubsystem::ScheduleTimer(this, &ATestTimerHolder::TimerFunction, { .Rate = 10.f });
+}
+
+void ATestTimerHolder::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	UTimerHolderSubsystem::CancelTimer(this, TimerHandle);
+	UTimerHolderSubsystem::CancelTimer(this);
+	UTimerHolderSubsystem::CancelTimer(this, ETimerScope::ContextBound);
+}
+
+void ATestTimerHolder::TimerFunction()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Start timer log in a function"))
+}
+
---
+++ Source/LogicraftCoreUtilsSB/Public/TestTimerHolder.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TestTimerHolder.generated.h"
+
+UCLASS()
+class LOGICRAFTCOREUTILSSB_API ATestTimerHolder : public AActor
+{
+	GENERATED_BODY()
+
+	FTimerHandle TimerHandle;
+	
+public:
+	ATestTimerHolder();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	
+	void TimerFunction();
+};
