ba91b654e74dec6b701946d41e14cf40c54f1d90
sharllesse <charles19lesage06@gmail.com>
2026-01-13 21:46:20 +0100

    Applied requested change

--- Plugins/LogicraftCoreUtils/Source/LogicraftCoreUtils/Private/TimerHolder.cpp
+++ Plugins/LogicraftCoreUtils/Source/LogicraftCoreUtils/Private/TimerHolder.cpp
@@ -2,10 +2,10 @@
 
 #include "TimerHolder.h"
 
-#define TIMER_HOLDER_CHECK \
-		checkf(TimerManager != nullptr, TEXT("The timer manager was not retrieved prior to using this function."));
+#define TIMER_HOLDER_CHECK() \
+		checkf(TimerManager != nullptr, TEXT("The timer manager was not retrieved prior to using this function."))
 
-#define TIMER_HOLDER_ENSURE \
+#define TIMER_HOLDER_ENSURE() \
 		ensureMsgf(RetrieveTimerManager(), TEXT("Unable to retrieve the timer manager because no valid context was found."))
 
 FTimerHolder::~FTimerHolder()
@@ -15,7 +15,7 @@ FTimerHolder::~FTimerHolder()
 
 void FTimerHolder::Pause()
 {
-	if (TIMER_HOLDER_ENSURE)
+	if (TIMER_HOLDER_ENSURE())
 	{
 		TimerManager->PauseTimer(TimerHandle);
 	}
@@ -23,7 +23,7 @@ void FTimerHolder::Pause()
 
 void FTimerHolder::Clear()
 {
-	if (TIMER_HOLDER_ENSURE)
+	if (TIMER_HOLDER_ENSURE())
 	{
 		TimerManager->ClearTimer(TimerHandle);
 	}
@@ -31,31 +31,31 @@ void FTimerHolder::Clear()
 
 bool FTimerHolder::IsPaused() const
 {
-	TIMER_HOLDER_CHECK
+	TIMER_HOLDER_CHECK();
 	return TimerManager->IsTimerPaused(TimerHandle);
 }
 
 bool FTimerHolder::IsAlreadyRunning() const
 {
-	TIMER_HOLDER_CHECK
+	TIMER_HOLDER_CHECK();
 	return TimerManager->TimerExists(TimerHandle);
 }
 
 float FTimerHolder::GetElapsedTime() const
 {
-	TIMER_HOLDER_CHECK
+	TIMER_HOLDER_CHECK();
 	return TimerManager->GetTimerElapsed(TimerHandle);
 }
 
 float FTimerHolder::GetRate() const
 {
-	TIMER_HOLDER_CHECK
+	TIMER_HOLDER_CHECK();
 	return TimerManager->GetTimerRate(TimerHandle);
 }
 
 float FTimerHolder::GetRemainingTime() const
 {
-	TIMER_HOLDER_CHECK
+	TIMER_HOLDER_CHECK();
 	return TimerManager->GetTimerRemaining(TimerHandle);
 }
 
