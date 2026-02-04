ecc8e9b5637b7eb4598833fdb4436c1bc292b834
Yann Grallan <grallanyann84@gmail.com>
2026-02-04 17:31:19 +0100

    Update project to UE 5.7 and build V6
    Bump EngineAssociation to 5.7 and update Editor target DefaultBuildSettings to BuildSettingsVersion.V6. Also correct the MustImplement metadata for ActorToSpawn to the fully-qualified interface path (/Script/LogicraftCoreUtils.Poolable) so the TSubclassOf enforces the intended Poolable interface. These changes align the project with UE 5.7 and the corresponding build settings.

--- Source/LogicraftCoreUtilsSB/Public/TestPoolSystem.h
+++ Source/LogicraftCoreUtilsSB/Public/TestPoolSystem.h
@@ -39,7 +39,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditDefaultsOnly, meta = (MustImplement = "Poolable"))
+	UPROPERTY(EditDefaultsOnly, meta = (MustImplement = "/Script/LogicraftCoreUtils.Poolable"))
 	TSubclassOf<AActor> ActorToSpawn;
 
 	UPROPERTY(Transient)
