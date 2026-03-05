f1061d26981b10abab15f567a5bd81a1561b42fb
Yann Grallan <grallanyann84@gmail.com>
2026-03-05 17:03:17 +0100

    Add FLCUHelper utility for default subobjects
    Add a new helper (FLCUHelper) to LogicraftCoreUtils that provides a templated CreateDefaultSubobject utility to simplify creating and attaching default subobjects. Adds header and minimal cpp files, includes CoreMinimal and Meta/LCUConcepts, and supports specifying a parent USceneComponent and transient flag when creating the subobject.

---
+++ Plugins/LogicraftCoreUtils/Source/LogicraftCoreUtils/Private/Utils/LCUHelper.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Logicraft Interactive. All Rights Reserved.
+
+
+#include "Utils/LCUHelper.h"
+
+
---
+++ Plugins/LogicraftCoreUtils/Source/LogicraftCoreUtils/Public/Utils/LCUHelper.h
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Logicraft Interactive. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Meta/LCUConcepts.h"
+
+struct LOGICRAFTCOREUTILS_API FLCUHelper
+{
+
+	template<Concept::DerivedFromObject ObjectType, typename InstantiateObject = std::remove_pointer_t<ObjectType>>
+	static void CreateDefaultSubobject(ObjectType& Object, const FName& ObjectName, USceneComponent* Parent = nullptr, bool bTransient = false)
+	{
+		Object = Object->template CreateDefaultSubobject<InstantiateObject>(ObjectName, bTransient);
+		if (Parent)
+		{
+			Object->SetupAttachment(Parent);
+		}
+	}
+};
